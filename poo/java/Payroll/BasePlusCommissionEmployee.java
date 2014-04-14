ba43248f0c64d28f0d23745837364ffa20dda41c
bla <nathan_cruz77@hotmail.com>
2014-04-14 00:19:24 -0300

    Finalizado exercício 1 da lista de sexta (poo)

--- poo/java/Payroll/BasePlusCommissionEmployee.java
+++ poo/java/Payroll/BasePlusCommissionEmployee.java
@@ -5,57 +5,35 @@ public class BasePlusCommissionEmployee extends CommissionEmployee{
     /* Atributos */
     private double baseSalary;
 
-   public BasePlusCommissionEmployee(String first, String last, 
+    /* Construtor */
+    public BasePlusCommissionEmployee(String first, String last, 
                                      String ssn, Date birthDate,
                                      int departmentCode, double sales,
                                      double rate, double salary){
-      super(first, last, ssn, birthDate, departmentCode, sales, rate);
-      setBaseSalary( salary ); // valida e armazena salário-base
-   } // fim do construtor BasePlusCommissionEmployee de seis argumentos
+        super(first, last, ssn, birthDate, departmentCode, sales, rate);
+        setBaseSalary(salary);
+    }
 
-   // configura o salário-base                                    
-   public void setBaseSalary( double salary )
-   {
-      baseSalary = ( salary < 0.0 ) ? 0.0 : salary; // não-negativo
-   } // fim do método setBaseSalary                         
+    /* Getters */
+    public double getBaseSalary()
+    {
+       return baseSalary;
+    }
 
-   // retorna o salário-base                                   
-   public double getBaseSalary()
-   {
-      return baseSalary;
-   } // fim do método getBaseSalary                            
+    /* Setters */
+    public void setBaseSalary( double salary ){
+        baseSalary = ( salary < 0.0 ) ? 0.0 : salary;
+    }        
 
-   // calcula os vencimentos; sobrescreve o método earnings em CommissionEmployee
-   public double earnings()                                             
-   {       
-        Date today = new Date();       
-
-        if(today.getMonth() == super.getPrivateBirthDate().getMonth())
-            return this.getBaseSalary() + super.earnings() + 150;
+    /* Calcula os ganhos mensais */
+    public double earnings(){
         return getBaseSalary() + super.earnings();                        
-   } // fim do método earnings              
+    }              
 
-   // retorna a representação String do objeto BasePlusCommissionEmployee
-   public String toString()                                            
-   {                                                                   
-      return String.format( "%s %s; %s: $%,.2f",                       
-         "base-salaried", super.toString(),                            
-         "base salary", getBaseSalary() );                             
-   } // fim do método toString                                            
-} // fim da classe BasePlusCommissionEmployee 
-
-
-/**************************************************************************
- * (C) Copyright 1992-2005 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+    /* Metodo invocado quando tentamos imprimir objetos desta classe */
+    public String toString(){                                                                   
+       return String.format("%s %s; %s: $%,.2f",                       
+                            "base-salaried", super.toString(),                            
+                            "base salary", getBaseSalary() );                             
+    }
+} 
--- poo/java/Payroll/SalariedEmployee.java
+++ poo/java/Payroll/SalariedEmployee.java
@@ -11,51 +11,33 @@ public class SalariedEmployee extends Employee{
                             double salary){
 
         super(first, last, ssn, birthDate, departmentCode); 
-        setWeeklySalary( salary ); 
+        this.setWeeklySalary(salary); 
 
-   } // fim do construtor SalariedEmployee com quatro argumentos
+    }
 
-   // configura o salário
-   public void setWeeklySalary( double salary )
-   {
-      weeklySalary = salary < 0.0 ? 0.0 : salary;
-   } // fim do método setWeeklySalary
+    /* Getters */
+    public double getWeeklySalary(){
+        return this.weeklySalary;
+    }
 
-   // retorna o salário
-   public double getWeeklySalary()
-   {
-      return weeklySalary;
-   } // fim do método getWeeklySalary
+    /* Setters */
+    public void setWeeklySalary(double salary){
+        this.weeklySalary = salary < 0.0 ? 0.0 : salary;
+    }
 
-   // calcula os rendimentos; sobrescreve o método earnings em Employee
-   public double earnings()                                            
-   {       
+    /* Calcula os ganhos do funcionario */
+    public double earnings()                                            
+    {       
         Date today = new Date();
         if(today.getMonth() == super.getPrivateBirthDate().getMonth())
             return this.getWeeklySalary()+150;
         return this.getWeeklySalary();
-   } // fim do método earnings              
+    }              
 
-   // retorna a representação String do objeto SalariedEmployee   
-   public String toString()                                     
-   {                                                            
-      return String.format( "salaried employee: %s\n%s: $%,.2f",
-         super.toString(), "weekly salary", getWeeklySalary() );
-   } // fim do método toString
-} // fim da classe SalariedEmployee
-
-
-/**************************************************************************
- * (C) Copyright 1992-2005 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+    /* Metodo invocado quando tentamos imprimir objetos desta classe */
+    public String toString()                                     
+    {                                                            
+        return String.format( "salaried employee: %s\n%s: $%,.2f",
+            super.toString(), "weekly salary", getWeeklySalary() );
+    }
+}
