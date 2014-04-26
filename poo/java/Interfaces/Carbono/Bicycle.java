de1ef007459064f2c190f28eeabca3ca7ae205b3
bla <nathan_cruz77@hotmail.com>
2014-04-26 17:27:09 -0300

    Iniciado exercício 2 de poo de sexta-feira

---
+++ poo/java/Interfaces/Carbono/Bicycle.java
@@ -0,0 +1,19 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+public class Bicycle{
+
+	/* Atributos */
+	private String modelo;
+	private String acessorios;
+
+	/* Construtor */
+	public Bicycle(String modelo, String acessorios){
+		this.modelo = modelo;
+		this.acessorios = acessorios;
+	}
+}
