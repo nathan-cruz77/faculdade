f3a756fa8e7dd96a740740d6b69082eeede83a38
bla <nathan_cruz77@hotmail.com>
2014-05-09 22:21:55 -0300

    Ecercicio 2

---
+++ poo/java/Collecions/Conta1/Conta.java
@@ -0,0 +1,46 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+public abstract class Conta {
+
+    /* Atributos */
+    protected double saldo;
+    protected int numero;
+
+    public void credita(double quantia) {
+        if (quantia > 0){
+            this.saldo += quantia;
+        }
+        else{
+            System.err.println("Não se pode depositar uma quantia negativa");
+        }
+    }
+
+    public void debita(double quantia) {
+        if (quantia>0){
+            this.saldo -= quantia;
+        }
+        else{
+            System.err.println("Não se pode sacar uma quantia negativa");
+        }
+    }
+
+	public abstract void atualiza(double taxa);
+
+    /* Getters */
+    public double getSaldo(){
+        return saldo;
+    }
+    public double getNumero(){
+        return numero;
+    }
+
+    public void imprime(){
+        System.out.println("Conta " + numero + ": " +
+                           "saldo = " + saldo );
+    }
+}
---
+++ poo/java/Collecions/Conta1/Tributavel.java
@@ -0,0 +1,10 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+public interface Tributavel{
+	public double calculaTributos();
+}
