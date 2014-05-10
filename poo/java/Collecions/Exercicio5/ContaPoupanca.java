f1f2d5a8c2206860a2521edb846c3a9d8cd66466
bla <nathan_cruz77@hotmail.com>
2014-05-10 04:02:37 -0300

    Finalizados exercicios em java

---
+++ poo/java/Collecions/Exercicio5/ContaPoupanca.java
@@ -0,0 +1,42 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+import java.lang.Comparable;
+
+public class ContaPoupanca extends Conta implements Comparable<ContaPoupanca>{
+
+	/* Atributos */
+	private String nomeDoCliente;
+
+    /* Construtor */
+    public ContaPoupanca(int numero, String nomeDoCliente){
+        this.numero = numero;
+		this.nomeDoCliente = nomeDoCliente;
+    }
+
+	/* Getters */
+	public String getNomeDoCliente(){
+		return this.nomeDoCliente;
+	}
+
+	/* Implementacao da interface */
+	public int compareTo(ContaPoupanca aux){
+		return this.getNomeDoCliente().compareTo(aux.getNomeDoCliente());
+	}
+
+	@Override
+    public void atualiza(double taxa){
+        this.saldo += 3*(this.saldo * taxa);
+    }
+
+	@Override
+	public void imprime(){
+		System.out.println("Conta " + this.numero + ": " +
+						   "titular = " + this.getNomeDoCliente() +
+                           " saldo = " + this.saldo );
+	}
+}
---
+++ poo/java/Collecions/Exercicio6/TestaPerformance.java
@@ -0,0 +1,120 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+import java.util.*;
+
+public class TestaPerformance {
+	public static void main(String[] args) {
+
+		System.out.println("Iniciando...");
+		long inicio_global = System.currentTimeMillis();
+		long fim_global;
+		long tempo_global;
+
+		long inicio = System.currentTimeMillis();
+
+		/* Inicializa estruturas de dados (collections) */
+		Collection<Integer> teste1 = new ArrayList<Integer>();
+		Collection<Integer> teste2 = new LinkedList<Integer>();
+		Collection<Integer> teste3 = new HashSet<Integer>();
+
+		/* Inicializa variaveis auxiliares */
+		int total = 30000;
+		long fim;
+		long tempo;
+
+		/* Exibe o tempo de inicializacao das variaveis */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\nTempo de inicializacao das variaveis: " + tempo + "ms");
+
+		/* Faz testes com a estrutura ArrayList */
+		System.out.println("\nTestes com a estrutura ArrayList:");
+		inicio = System.currentTimeMillis();
+
+		/* Popula o ArrayList */
+		for (int i = 0; i < total; i++) {
+			teste1.add(i);
+		}
+
+		/* Imprime o tempo de preenchimento da estrutura ArrayList */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de preenchimento: " + tempo + "ms");
+
+		inicio = System.currentTimeMillis();
+		/* Pesquisa os elementos no vetor */
+		for (int i = 0; i < total; i++) {
+			teste1.contains(i);
+		}
+
+		/* Imprime o tempo de pesquisa na estrutura ArrayList */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de pesquisa: " + tempo + "ms");
+
+
+
+		/* Faz testes com a estrutura LinkedList */
+		System.out.println("\nTestes com a estrutura LinkedList:");
+		inicio = System.currentTimeMillis();
+
+		/* Popula o LinkedList */
+		for (int i = 0; i < total; i++) {
+			teste2.add(i);
+		}
+
+		/* Imprime o tempo de preenchimento da estrutura LinkedList */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de preenchimento: " + tempo + "ms");
+
+		inicio = System.currentTimeMillis();
+		/* Pesquisa os elementos no vetor */
+		for (int i = 0; i < total; i++) {
+			teste2.contains(i);
+		}
+
+		/* Imprime o tempo de pesquisa na estrutura LinkedList */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de pesquisa: " + tempo + "ms");
+
+
+
+		/* Faz testes com a estrutura HashSet */
+		System.out.println("\nTestes com a estrutura HashSet:");
+		inicio = System.currentTimeMillis();
+
+		/* Popula o HashSet */
+		for (int i = 0; i < total; i++) {
+			teste3.add(i);
+		}
+
+		/* Imprime o tempo de preenchimento da estrutura HashSet */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de preenchimento: " + tempo + "ms");
+
+		inicio = System.currentTimeMillis();
+		/* Pesquisa os elementos no vetor */
+		for (int i = 0; i < total; i++) {
+			teste3.contains(i);
+		}
+
+		/* Imprime o tempo de pesquisa na estrutura ArrayList */
+		fim = System.currentTimeMillis();
+		tempo = fim - inicio;
+		System.out.println("\tTempo de pesquisa: " + tempo + "ms");
+
+		/* Imprime tempo total gasto */
+		fim_global = System.currentTimeMillis();
+		tempo_global = fim_global - inicio_global;
+		System.out.println("\nTempo total: " + tempo_global + "ms");
+	}
+}
+
