4f51c0e26712d48960a5f73365892c6e9b0adf45
bla <nathan_cruz77@hotmail.com>
2014-05-09 22:21:55 -0300

    Ecercicio 2

---
+++ poo/java/Collecions/Conta2/ContaCorrente.java
@@ -0,0 +1,38 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 04/04/14
+ *  R.A.: 86492
+ */
+
+public class ContaCorrente extends Conta implements Tributavel{
+
+    /* Construtor */
+    public ContaCorrente(int numero){
+        this.numero = numero;
+    }
+
+	@Override
+    public void atualiza(double taxa){
+        this.saldo += 2*(this.saldo * taxa);
+    }
+
+    @Override
+    public void credita(double quantia) {
+        if (quantia > 0){
+            this.saldo += quantia;
+            this.saldo -= 0.10;
+        }
+        else{
+            System.err.println("Não se pode depositar uma quantia negativa");
+        }
+    }
+
+	/* Implementacao da interface */
+	public double calculaTributos(){
+		if(this.saldo > 0)
+			return (this.saldo/100);
+		System.out.println("Valor do saldo invalido");
+		return 0;
+	}
+}
