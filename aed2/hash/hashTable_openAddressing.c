abc8819f44abb9782bc101c427717fb8291010e9
bla <nathan_cruz77@hotmail.com>
2014-05-31 19:04:18 -0300

    Exercicios de HASH

---
+++ aed2/hash/hashTable_openAddressing.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NIL -1
+
+typedef int TChave;
+
+typedef struct {
+	TChave Chave;
+	/* outros compomentes */
+} TItem;
+
+typedef int TIndice;
+
+typedef struct {
+	TItem *Item;
+	TIndice m, n;
+} THash;
+
+TIndice h(THash *T, TChave k)
+{
+	return k % T->m;
+}
+
+TIndice H(THash *T, TChave k, int i)
+{
+  return (h(T, k) + i) % T->m;
+}
+
+THash *Inicializa(int m)
+{
+	TIndice i;
+	THash *T;
+
+	T = (THash *) malloc(sizeof(THash));
+
+	T->n = 0; T->m = m;
+	T->Item = (TItem *) malloc(T->m * sizeof(TItem));
+	for (i = 0; i < T->m; i++)
+		T->Item[i].Chave = NIL;
+
+	return T;
+}
+
+TIndice Pesquisa(THash *T, TChave x){
+	int i;
+    TIndice aux = H(T, x, 0);
+
+    for(i=1; i<(T->m) && T->Item[aux].Chave != x; i++){
+        aux = H(T, x, i);
+    }
+
+    if(T->Item[aux].Chave == x) return aux;
+    return NIL;
+}
+
+int Insere(THash *T, TItem x)
+{
+    int i;
+    TIndice aux = H(T, x.Chave, 0);
+
+    for(i=1; i<(T->m) && T->Item[aux].Chave != NIL; i++){
+        aux = H(T, x.Chave, i);
+    }
+
+    if(T->Item[aux].Chave == NIL){
+        T->Item[aux] = x;
+        T->n++;
+        return 1;
+    }
+    return 0;
+}
+
+void Carrega(THash *T)
+{
+	int i, n;
+	TItem x;
+
+	scanf("%d", &n);
+	for (i = 0; i < n ; i++) {
+		scanf("%d", &x.Chave);
+		Insere(T, x);
+	}
+}
+
+void Libera(THash **T)
+{
+	THash *aux;
+	int i;
+
+	aux = *T;
+	if (aux != NULL) {
+		if (aux->Item != NULL) free(aux->Item);
+		free(aux);
+		(*T) = NULL;
+	}
+}
+
+void Imprime(THash *T)
+{
+	int i;
+	if (T->m > 0) {
+		for (i = 0; i < T->m; i++)
+			printf("[%d] %d\n", i, T->Item[i].Chave);
+	}
+}
+
+int main()
+{
+	TIndice m;
+	THash *T;
+	TItem x;
+
+	scanf("%d", &m);
+	T = Inicializa(m);
+	Carrega(T);
+	scanf("%d", &x.Chave);
+	if (Pesquisa(T, x.Chave) == NIL)
+		Insere(T, x);
+	Imprime(T);
+	Libera(&T);
+
+	return 0;
+}
---
+++ poo/java/Static/Exercicio1/DemoDataComFabrica.java
@@ -0,0 +1,41 @@
+/*
+ *  Programa escrito com propósito didático apenas.
+ *  @author Nathan Cruz <nathan.cruz@unifesp.br>
+ *  @created 25/03/14
+ *  R.A.: 86492
+ *
+ *  Professor: Todos os anos estao sendo impressos como 0.
+ *  Nao sei o motivo. Se souber, por favor, me fale.
+ */
+
+public class DemoDataComFabrica{
+	public static void main(String args[]){
+		DataComFabrica Natal2De1966, Natal2De1970, Natal2De2000;
+		DataComFabrica SeteDe1966, SeteDe1970, SeteDe2000;
+		DataComFabrica PrimeiroDe1966, PrimeiroDe1970, PrimeiroDe2000;
+
+		Natal2De1966 = DataComFabrica.Natal2((short) 1966);
+		Natal2De1970 = DataComFabrica.Natal2((short) 1970);
+		Natal2De2000 = DataComFabrica.Natal2((short) 2000);
+
+		SeteDe1966 = DataComFabrica.seteDeSetembro((short) 1966);
+		SeteDe1970 = DataComFabrica.seteDeSetembro((short) 1970);
+		SeteDe2000 = DataComFabrica.seteDeSetembro((short) 2000);
+
+		PrimeiroDe1966 = DataComFabrica.primeiroDoMes((byte) 3, (short) 1966);
+		PrimeiroDe1970 = DataComFabrica.primeiroDoMes((byte) 5, (short) 1970);
+		PrimeiroDe2000 = DataComFabrica.primeiroDoMes((byte) 7, (short) 2000);
+
+		System.out.println(Natal2De1966);
+		System.out.println(Natal2De1970);
+		System.out.println(Natal2De2000);
+
+		System.out.println(SeteDe1966);
+		System.out.println(SeteDe1970);
+		System.out.println(SeteDe2000);
+
+		System.out.println(PrimeiroDe1966);
+		System.out.println(PrimeiroDe1970);
+		System.out.println(PrimeiroDe2000);
+	}
+}
