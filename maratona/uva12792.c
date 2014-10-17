d5975ec6b186247480cefadb40a405f6aed83aba
bla <nathan_cruz77@hotmail.com>
2014-10-17 20:04:01 -0300

    Exercicio 12792 do UVa, tentativa 1

--- maratona/uva12792.c
+++ maratona/uva12792.c
@@ -3,43 +3,15 @@
 #include <stdbool.h>
 
 bool isOrdenado(int* A, int N){
-    int i;
 
-    for(i=0; i<N-1; i++){
-        if(A[i] > A[i+1]){
-            return false;
-        }
+    if(A[0] != 1){
+        return false;
     }
-
     return true;
-}
-
-int* shuffle(int* A, int N){
-    int B[N];
-    int i, j;
-
-    B[1] = A[0];
-    for(i=1; i<=N/2; i++){
-        B[i+2] = A[i];
-    }
-
-    for(i=N/2+1, j=0; i<N; i++, j+=2){
-        B[j] = A[i];
-    }
-
-    for(i=0; i<N; i++){
-        A[i] = B[i];
-    }
-
-    free(A);
-
-    return B;
 }
 
 void generator(int* A, int N){
     int i;
-
-    A = (int*) malloc(sizeof(int)*N);
 
     for(i=0; i<N; i++){
         A[i] = i+1;
@@ -56,22 +28,43 @@ void print_vec(int* A, int N){
     printf("\n");
 }
 
+void shuffle(int* A, int N, int cont){
+    int B[N];
+    int i, p;
+
+    for(i=0, p=N/2; i<N; i+=2, p++){
+        B[i] = A[p];
+    }
+
+    for(i=1, p=0; i<N; i+=2, p++){
+        B[i] = A[p];
+    }
+
+    for(i=0; i<N; i++){
+        A[i] = B[i];
+    }
+}
+
 int main(){
-    int N, cont;
+    int N, cont, i;
     int* A;
+    double tempo;
+
+    A = (int*) malloc(sizeof(int)*100000);
 
     while(scanf("%d", &N)){
         generator(A, N);
-        //print_vec(A, N);
-        A = shuffle(A, N);
-        //print_vec(A, N);
+        shuffle(A, N, 1);
         cont = 1;
+
         while(!isOrdenado(A, N)){
-            A = shuffle(A, N);
+            shuffle(A, N, cont+1);
             cont++;
         }
-        free(A);
+        printf("%d\n", cont);
+
     }
+    free(A);
 
     return 0;
 }
