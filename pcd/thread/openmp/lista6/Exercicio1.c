93e17edf484863b974332b503db6327b713a6935
bla <nathan_cruz77@hotmail.com>
2014-10-29 17:53:03 -0200

    Consertada implementacao do desvio padrao com OpenMP usando barreiras.

--- pcd/thread/openmp/lista6/Exercicio1.c
+++ pcd/thread/openmp/lista6/Exercicio1.c
@@ -5,7 +5,7 @@
 #include <omp.h>
 
 #define TAMANHO 100000000 //10**8
-#define MAX_THREADS 4
+#define MAX_THREADS 8
 
 /* Vetor de dados */
 double* dados;
@@ -14,6 +14,7 @@ int main(){
     int i;
     double media, result, aux;
     double somatorio, size, tempo;
+    double somatorio_parcial=0;
 
     /* Semente para o random */
     srand(time(NULL));
@@ -35,13 +36,17 @@ int main(){
     somatorio = 0;
     omp_set_num_threads(MAX_THREADS);
     #pragma omp parallel shared(dados, media, somatorio) private(i)
+    #pragma omp firstprivate(somatorio_parcial)
     {
         #pragma omp for
         for(i=0; i<TAMANHO; i++)
-            #pragma omp critical
-            somatorio += dados[i];
+            somatorio_parcial += dados[i];
 
-        //#pragma omp barrier
+
+        #pragma omp critical
+            somatorio += somatorio_parcial;
+
+        #pragma omp barrier
 
         #pragma omp single
         {
@@ -49,12 +54,16 @@ int main(){
             somatorio = 0;
         }
 
-        //#pragma omp barrier
+        #pragma omp barrier
+
+        somatorio_parcial = 0;
 
         #pragma omp for
         for(i=0; i<TAMANHO; i++)
-            #pragma omp critical
-            somatorio += pow(dados[i] - media, 2);
+            somatorio_parcial += pow(dados[i] - media, 2);
+
+        #pragma omp critical
+            somatorio += somatorio_parcial;
     }
     tempo = omp_get_wtime() - tempo;
     printf("Tempo de calculo do desvio padrao: %.3lfs\n", tempo);
