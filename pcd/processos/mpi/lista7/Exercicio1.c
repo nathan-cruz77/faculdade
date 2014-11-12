bad71c5be50ad24ad1435d77860bc09e558c4520
spock <spock@enterprise.home>
2014-11-12 17:44:32 -0200

    Ultimos exercicios de PC

--- pcd/processos/mpi/lista7/Exercicio1.c
+++ pcd/processos/mpi/lista7/Exercicio1.c
@@ -17,7 +17,9 @@ int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
 
     /* Captura o tempo do sistema */
-    tempo = MPI_Wtime();
+	if(rank == 0){
+    	tempo = MPI_Wtime();
+	}
 
     /* Detecta o rank do processo corrente e a quantidade total de processos */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -28,34 +30,20 @@ int main(int argc, char* argv[]) {
     fim = ini + (N/size);
 
     passo = (u-1) / (double) N;
+	soma = 0;
+	soma_escravo = 0;
 
     for (i=ini; i<fim; i++) {
         x=1+i*passo;
-        soma += 0.5*(1/x+1/(x+passo));
+        soma_escravo += 0.5*(1/x+1/(x+passo));
     }
 
-    /* Se estivermos no processo mestre, aguardamos o resultado da computacao
-     * de todos os escravos e geramos o resultado final */
-    if(rank == 0){
+	MPI_Reduce(&soma_escravo, &soma, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-        /* Recebemos o resultado de todos */
-        for(i=0; i<size-1; i++){
-            MPI_Recv(&soma_escravo, 1, MPI_DOUBLE, MPI_ANY_SOURCE,
-                     MPI_ANY_TAG, MPI_COMM_WORLD, &aux);
-
-            /* Verificar a possibilidade de fazer reducao */
-            soma += soma_escravo;
-        }
-        tempo = MPI_Wtime() - tempo;
-
+	if(rank == 0){
+		tempo = MPI_Wtime() - tempo;
         printf("ln %f = %20.15f\n", u, passo*soma);
         printf("Tempo da computacao: %.3lfs\n", tempo);
-    }
-
-    /* Se estivermos num processo escravo enviamos o resultado parcial para
-     * o mestre */
-    else{
-        MPI_Send(&soma, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
--- pcd/processos/mpi/lista7/Exercicio2.c
+++ pcd/processos/mpi/lista7/Exercicio2.c
@@ -11,12 +11,17 @@
 void main(int argc, char* argv[]) {
 
     double *tmp, *u, *u_prev;
-    double x, t, tempo;
-    long int i, n, maxloc, ini, fim;
-    int rank, size;
+    double x, t, tempo, maxval, maxval_global;
+    long int i, n, maxloc, ini, fim, maxloc_global;
+    int rank, size, prox, ant;
+	MPI_Status status;
 
     /* Inicializa o MPI */
     MPI_Init(&argc, &argv);
+
+	if(rank == 0){
+		tempo = MPI_Wtime();
+	}
 
     /* Detecta o rank do processo corrente e o total de processos */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -25,12 +30,9 @@ void main(int argc, char* argv[]) {
     /* Claculando quantidade de pontos */
     n = (tam/dx)/size;
 
-    ini = (n)*rank;
-    fim = ini + (n);
-
     /* Alocando vetores */
     u = (double *) malloc((n + 1)*sizeof(double));
-    u_prev = (double *) malloc((n + 3)*sizeof(double));
+    u_prev = (double *) malloc((n + 1)*sizeof(double));
 
     /* Apenas o processo mestre imprime */
     if(rank == 0){
@@ -58,36 +60,95 @@ void main(int argc, char* argv[]) {
         x += dx;
     }
 
+	/* Calcula os processos com os quais interagir */
+	prox = rank + 1;
+	ant = rank - 1;
+
+	/* Ajusta o proximo do ultimo e o anterior do primeiro */
+	if(rank == 0){
+		ant = MPI_PROC_NULL;
+	}
+	else if(rank == size -1){
+		prox = MPI_PROC_NULL;
+	}
+
     t = 0.;
     while (t<T) {
+
+		/* Calcula o novo u */
         x = dx;
         for (i=1; i<n; i++) {
             u[i] = u_prev[i] + kappa*dt/(dx*dx)*(u_prev[i-1]-2*u_prev[i]+u_prev[i+1]);
             x += dx;
         }
-        u[0] = u[n] = 0.; /* forca condicao de contorno */
-        tmp = u_prev; u_prev = u; u = tmp; /* troca entre ponteiros */
+		
+		/* Envia para o proximo e recebe do anterior */
+		MPI_Sendrecv(&u[n-1], 1, MPI_DOUBLE, prox, 0,
+					 &u_prev[0], 1, MPI_DOUBLE, ant, MPI_ANY_TAG,
+					 MPI_COMM_WORLD, &status);
+
+		/* Envia para o anterior e recebe do proximo */
+		MPI_Sendrecv(&u[1], 1, MPI_DOUBLE, ant, 0,
+					 &u_prev[n], 1, MPI_DOUBLE, prox, MPI_ANY_TAG,
+					 MPI_COMM_WORLD, &status);
+	
+		/* Forca condicao de contorno */
+		if(rank == 0){
+        	u[0] = 0.;
+		}
+		else if(rank == size-1){
+			u[n] = 0.;
+		}
+
+		/* Copia tudo de u para u_prev */
+		for(i=1; i<n; i++){
+			u_prev[i] = u[i];
+		}
         t += dt;
     }
 
-    tempo = MPI_Wtime() - tempo;
-
-    if(rank == 0){
-        printf("Tempo do segundo laco: %.3lfs\n", tempo);
-    }
-
-    tempo = MPI_Wtime();
     /* Calculando o maior valor e sua localizacao */
     maxloc = 0;
     for (i=1; i<n+1; i++) {
         if (u[i] > u[maxloc]) maxloc = i;
     }
 
-    tempo = MPI_Wtime() - tempo;
+	maxval = u[maxloc];
+	maxloc = (rank * (n+1)) + maxloc;
+
+	if(rank != 0){
+		double* data = (double*) malloc(2*sizeof(double));
+		MPI_Send(data, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	}
+	else{
+
+		double** data = (double**) malloc(size*sizeof(double*));
+		for(i=0; i<size; i++){
+			data[i] = (double*) malloc(2*sizeof(double));
+		}
+
+		data[0][0] = maxval;
+		data[0][1] = maxloc;
+
+		for(i=1; i<size; i++){
+			MPI_Recv(data[i], 2, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
+					 MPI_COMM_WORLD, &status);
+		}
+
+		maxval_global = maxval;
+		maxloc_global = maxloc;
+		for(i=0; i<size; i++){
+			if(data[i][0] > maxval_global){
+				maxval_global = data[i][0];
+				maxloc_global = data[i][i];
+			}
+		}
+	}
 
     if(rank == 0){
-        printf("Tempo do terceiro laco: %.3lfs\n", tempo);
-        printf("Maior valor u[%ld] = %g\n", maxloc, u[maxloc]);
+    	tempo = MPI_Wtime() - tempo;
+        printf("Tempo total: %.3lfs\n", tempo);
+        printf("Maior valor u[%ld] = %g\n", maxloc_global, maxval_global);
     }
 
     MPI_Finalize();
