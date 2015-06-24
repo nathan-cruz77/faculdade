8d0b561b19433c21a1b8e4e03421fb9c4a656094
Nathan Cruz <nathan.cruz@unifesp.br>
2015-06-24 17:06:41 -0300

    Tentativa 1 de fazer o ultimo exercicio de AED2

--- aed2/vez_2/novos/maior_poligono_regular.c
+++ aed2/vez_2/novos/maior_poligono_regular.c
@@ -1,28 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int hash(int k, int m){
-	return k % m;
+bool hash(int* pontos, bool* tabela, int i, int distancia, int perimetro){
+	int indice = ((distancia + pontos[i]) % perimetro) - 1;
+
+	if(tabela[indice]){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
-void carrega(int** pontos, int* n, int* r){
+void inicializa_tabela(bool** tabela, int n, int* pontos, int np){
 	int i;
 
-	scanf("%d", r);
+	*tabela = malloc(sizeof(bool) * n);
+
+	for(i=0; i<n; i++){
+		(*tabela)[i] = false;
+	}
+
+	for(i=0; i<np; i++){
+		(*tabela)[pontos[i]-1] = true;
+	}
+}
+
+void carrega(int** pontos, int* n, int* p){
+	int i;
+
+	scanf("%d", p);
 	scanf("%d", n);
 
-	*pontos = malloc(sizeof(int) * (*r));
+	*pontos = malloc(sizeof(int) * (*p));
 
 	for(i=0; i<(*n); i++){
 		scanf("%d", &(*pontos[i]));
 	}
 }
 
+void descobre_razao(int* vet, int p, int n, bool* tabela, int* ini_pt, int* razao){
+	int r, i, j;
+	
+	*razao = -1;
+	*ini_pt = -1;
+
+	for(i=0; i<p; i++){
+		for(r=1; r<= p/3; r++){
+			for(j=1; j<=n && hash(vet, tabela, i, r*j, p); j++);
+			if(j < n){
+				*razao = r;
+				*ini_pt = i;
+				return;
+			}
+		}
+	}
+}
+
 int main(){
 	int* pontos;
-	int n, r;
+	int n, p, r;
+	bool* tabela;
+	int ini_pt, razao;
 
-	carrega(&pontos, &n, &r);
+	carrega(&pontos, &n, &p);
+	inicializa_tabela(&tabela, p, pontos, n);
+
+	descobre_razao(pontos, p, n, tabela, &ini_pt, &razao);
+
+	printf("%d %d\n", ini_pt, razao);
 
 	return 0;
 }
