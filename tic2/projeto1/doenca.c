7c6dd40a30523257269ff24f727ee20cfaf34723
Nathan Cruz <nathan.cruz@unifesp.br>
2015-10-05 22:38:53 -0300

    Tentativa de corrigir o programa de Topicos Interdisciplinares em Computacao

--- tic2/projeto1/doenca.c
+++ tic2/projeto1/doenca.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <string.h>
 #define N 1
-#define tmax 10
+#define tmax 100.
 #define dt 0.01
 
 //Constantes
@@ -18,18 +18,18 @@
 
 
 void le_entrada(char** args, double* gamaT, double* delta, double* I){
-	*gamaT =  (double) strtod(args[1], (char**) NULL);
-	*delta =  (double) strtod(args[2], (char**) NULL);
-	*I =  (double) strtod(args[3], (char**) NULL);
-	
+    *gamaT =  (double) strtod(args[1], (char**) NULL);
+    *delta =  (double) strtod(args[2], (char**) NULL);
+    *I =  (double) strtod(args[3], (char**) NULL);
+
 }
 
 void Doenca(double delta, double gamaT, double psiT, double S, double I){
-	double dS, dE, dI, dR, dV, gama=0, psi=0;
-	double E=0, R=0, V=0;
-	double t;
-	
-	//Arquivo para o grafico
+    double dS, dE, dI, dR, dV, gama=0, psi=0;
+    double E=0, R=0, V=0;
+    double t;
+
+    //Arquivo para o grafico
     char impr_doenca[] = "doenca.txt";
     FILE *arq;
     //Abertura do arquivo
@@ -39,56 +39,68 @@ void Doenca(double delta, double gamaT, double psiT, double S, double I){
         printf("Erro, nao foi possivel abrir o arquivo\n");
         return ;
     }
-	
-	for(t=0; t<tmax; t+=dt){
 
-	    //Verifica o quanto de populacao que ira nascer
-	    gama = (omega_1*(S+E+R+V) + omega_2*I)*gamaT;
-	    psi = (omega_1*(S+E+R+V) + omega_2*I)*psiT;
+    for(t=dt; t<tmax; t+=dt){
 
-	    //Taxa de variacao da populacao sucetivel
-	    dS = (lambda*R + gama - mu*I*S - omega_1*S - delta*S - sigma*E*S)*t;
-	    
-	    //Taxa de variacao da populacao exposta
-	    dE = (mu*I*S + alfa*I + sigma*E*S - omega_1*E - beta*E - zeta*E)*t;
-	    
-	    //Taxa de variacao da populacao infectada
-	    dI = (zeta*E - omega_2*I - alfa*I)*t;
-	    
-	    //Taxa variacao da populacao recuperada
-	    dR = (beta*E - lambda*R - omega_1*R)*t;
-	    
-	    //Taxa de variacao da populacao vacinada
-	    dV = (delta*S + psi - omega_1*V)*t;
-	    
-	    //Acrescenta a taxa na populacao correspondente
-	    S += dS;
-	    E += dE;
-	    I += dI;
-	    R += dR;
-	    V += dV;
+        //Verifica o quanto de populacao que ira nascer
+        gama = (omega_1*(S+E+R+V) + omega_2*I)*gamaT;
+        psi = (omega_1*(S+E+R+V) + omega_2*I)*psiT;
 
-	    /*printf("S = %f\n",S);
-	    printf("E = %f\n",E);
-	    printf("I = %f\n",I);
-	    printf("R = %f\n",R);
-	    printf("V = %f\n",V);
-	    printf("S+E+I+R+V = %f\n",S+E+I+R+V);*/
+        //Taxa de variacao da populacao sucetivel
+        dS = (lambda*R + gama - mu*I*S - omega_1*S - delta*S - sigma*E*S)*t;
 
-	    fprintf(arq,"%f\t%f\t%.2f\n",S,I, t);
-	}
+        //Taxa de variacao da populacao exposta
+        dE = (mu*I*S + alfa*I + sigma*E*S - omega_1*E - beta*E - zeta*E)*t;
+
+        //Taxa de variacao da populacao infectada
+        dI = (zeta*E - omega_2*I - alfa*I)*t;
+
+        //Taxa variacao da populacao recuperada
+        dR = (beta*E - lambda*R - omega_1*R)*t;
+
+        //Taxa de variacao da populacao vacinada
+        dV = (delta*S + psi - omega_1*V)*t;
+
+        fprintf(stdout, "%f\t%f\t%f\t%f\t%f\t%.2f\n", dS, dE, dI, dR, dV, t);
+        fprintf(stdout, "%lf\t%lf\t%lf\t%lf\t%lf\t%.2lf\n", fabs(dS), fabs(dE), fabs(dI), fabs(dR), fabs(dV), t);
+        printf("\n");
+
+        /* Se chegou a um ponto de equilibrio, pare a simulacao */
+        if(fabs(dS) == 0.0 && fabs(dE) == 0.0 && fabs(dI) == 0.0 && fabs(dR) == 0.0 && fabs(dV) == 0.0){
+            t = tmax;
+            fprintf(stdout, "%lf\t%lf\t%lf\t%lf\t%lf\t%.2lf\n", fabs(dS), fabs(dE), fabs(dI), fabs(dR), fabs(dV), t);
+        }
+        else{
+
+            //Acrescenta a taxa na populacao correspondente
+            S += dS;
+            E += dE;
+            I += dI;
+            R += dR;
+            V += dV;
+
+            /*printf("S = %f\n",S);
+            printf("E = %f\n",E);
+            printf("I = %f\n",I);
+            printf("R = %f\n",R);
+            printf("V = %f\n",V);
+            printf("S+E+I+R+V = %f\n",S+E+I+R+V);*/
+
+            fprintf(arq,"%f\t%f\t%f\t%f\t%f\t%.2f\n", S, E, I, R, V, t);
+        }
+    }
 }
 
 void main(int a, char** args){
-	double gamaT; //Porcentagem de pessoas que nascem e nao sao vacinadas
-	double delta; //Porcentagem de pessoas que sao vacinas adultas
-	double psiT;
-	double S; //Porcentagem de pessoas sucetiveis
-	double I; //Porcentagem de pessoas infectadas
+    double gamaT; //Porcentagem de pessoas que nascem e nao sao vacinadas
+    double delta; //Porcentagem de pessoas que sao vacinas adultas
+    double psiT;
+    double S; //Porcentagem de pessoas sucetiveis
+    double I; //Porcentagem de pessoas infectadas
 
-	le_entrada(args, &gamaT, &delta, &I);
+    le_entrada(args, &gamaT, &delta, &I);
     psiT = 1 - gamaT;
     S = 1 - I;
-	
-	Doenca(delta,gamaT,psiT,S,I);
+
+    Doenca(delta,gamaT,psiT,S,I);
 }
