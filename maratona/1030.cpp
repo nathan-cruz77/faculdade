896ed0a56fa495093f0abfb9f1762a520616f83f
bla <nathan_cruz77@hotmail.com>
2015-11-07 15:46:01 -0200

    Finalizado exercicio do problema de Josephus (1030 do URI).

--- maratona/1030.cpp
+++ maratona/1030.cpp
@@ -4,46 +4,24 @@
 
 using namespace std;
 
-int pos_um_vivo(vector<int> vet){
-	int i;
-
-	for(i=0; i<vet.size(); i++){
-		if(vet[i] == 0){
-			return i;
-		}
-	}
-
-	return -1;
+int josephus(int n, int k)
+{
+  if (n == 1)
+    return 1;
+  else
+    return (josephus(n - 1, k) + k-1) % n + 1;
 }
 
 int main(){
-	int i, j, mortos;
-	int n, c, k;
-	int aux;
-	vector<int> vet;
+    int n, c, k;
+    int i, j;
 
-	cin >> n;
-	for(i=0; i<n; i++){
-		cin >> c;
-		cin >> k;
+    cin >> n;
+    for(i=0; i<n; i++){
+        cin >> c >> k;
 
-		vet.clear();
-		vet.resize(c);
-		fill(vet.begin(), vet.end(), 0);
+        cout << "Case " << i+1 << ": " << josephus(c, k) << endl;
+    }
 
-		mortos = 0;
-		for(j=0; mortos != c-1; j+=k){
-			aux = j % c;
-			if(vet[aux] == 0){
-				vet[aux] = 1;
-				mortos++;
-			}
-			cout << "Mortos: " << mortos << "\tj: " << j
-					<< "\taux: " << aux << endl;
-
-		}
-
-		cout << "Case " << i+1 << ": " <<  pos_um_vivo(vet) << endl;
-	}
-	return 0;
+    return 0;
 }
