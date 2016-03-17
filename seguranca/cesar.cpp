8818b9fed45bff535da732711f056ff9fbd592c2
Nathan Cruz <nathan.cruz@unifesp.br>
2016-03-17 15:00:43 -0300

    Exercicios de criptografia

---
+++ seguranca/cesar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	int n;
+	string entrada;
+
+	cin >> n;
+	cin >> entrada;
+
+	for(int i = 0; i < entrada.size(); i++){
+		entrada[i] = toupper(entrada[i]);
+
+		entrada[i] = (entrada[i] + n) < 90 ? (entrada[i] + n) : (entrada[i] + n) - 26;
+	}
+
+	cout << entrada << endl;
+
+	return 0;
+}
---
+++ seguranca/vegenere.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char cesar(char c, int n){
+	c = toupper(c);
+	c = (c + n) <= 90 ? (c + n) : (c + n) - 26;
+
+	return c;
+}
+
+int main(){
+
+	string chave;
+	string entrada;
+	string saida;
+	int deslocamento = 0;
+
+	cin >> chave;
+	cin >> entrada;
+
+	for(int i = 0; i < entrada.size(); i++){
+
+		if(entrada[i] == ' '){
+			saida += ' ';
+		}
+		else{
+			deslocamento = (int) toupper(chave[i % chave.size()]) - 65;
+			saida += cesar(entrada[i], deslocamento);
+		}
+	}
+
+	cout << saida << endl;
+
+	return 0;
+}
