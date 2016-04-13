e0da4832cbd7226b91f50001ec720d15df109fdb
Nathan Cruz <nathan.cruz@unifesp.br>
2016-04-12 23:52:47 -0300

    Separados os modos server e client do secure chat.

--- seguranca/chat/chat.cpp
+++ seguranca/chat/chat.cpp
@@ -26,13 +26,18 @@
 using namespace std;
 
 ev_io* client_watcher;
-char* buffer;
+
+char* buffer_send;
+char* buffer_recv;
+
 string key;
 
 typedef struct{
+
 	char* server_ip;
 	int server_port;
-	ev_io* this_server;
+	int client_fd;
+
 } connection_data;
 
 
@@ -43,15 +48,32 @@ void make_non_blocking(int fd){
 }
 
 
-void check_input(int size, char** args){
+bool check_input(int size, char** args){
 
-	if(size != 4){
-		printf("[usage]: %s <ip> <port> <key>\n\n", args[0]);
+	bool server_mode;
+
+	if(size != 4 && size != 2){
+		printf("[usage]: %s <ip> <port> <key>\t// Client mode\n", args[0]);
+		printf("         OR\n");
+		printf("         %s <key>\t\t\t// Server mode\n\n", args[0]);
 		printf("IP:\tIp of secure-chat server\n");
-		printf("PORT:\tPort where the server will listen for connections\n");
+		printf("PORT:\tPort of secure-chat server\n");
 		printf("Key:\tKey used for encription\n");
+
 		exit(EXIT_FAILURE);
 	}
+
+	if(size == 4){
+		printf("Initializing client mode.\n");
+		server_mode = false;
+	}
+
+	if(size == 2){
+		printf("Initializing server mode\n");
+		server_mode = true;
+	}
+
+	return server_mode;
 
 }
 
@@ -117,32 +139,40 @@ void client_callback(EV_P_ ev_io* watcher, int events){
 	size_t i = 0;
 
 	if(events & EV_READ){
-		amount_read = recv(watcher->fd, buffer, 64 KB, 0);
+		memset(buffer_recv, 0, 64 KB);
+		fgets(buffer_send, 64 KB, stdin);
 
-		string aux(buffer);
-		aux = rc4(key, aux);
+		amount_read = recv(watcher->fd, buffer_recv, 64 KB, 0);
+
+		/*string aux(buffer_recv);
+		aux = rc4(key, aux);*/
+
+		if(amount_read <= 0)
+			return;
 
 		printf("\r<<< ");
 		for(i = 0; i < amount_read; i++)
-			printf("%c", aux[i]);
-		printf("\n>>> ");
+			printf("%c", buffer_recv[i]);
+		printf("\n>>> %s", buffer_send);
 	}
 
 	if(events & EV_WRITE){
-		memset(buffer, 0, 64 KB);
-		fgets(buffer, 64 KB, stdin);
+		memset(buffer_send, 0, 64 KB);
+		fgets(buffer_send, 64 KB, stdin);
 
-		string aux(buffer);
-		aux = rc4(key, aux);
+		/*string aux(buffer);
+		aux = rc4(key, aux);*/
 
-		amount_read = aux.size();
+		amount_read = strlen(buffer_send);
 
-		for(i = 0; i < aux.size(); i++){
+		/*
+		for(i = 0; i < amount_read; i++){
 			buffer[i] = aux[i];
 		}
+		*/
 
 		if(amount_read > 0){
-			send(watcher->fd, buffer, amount_read - 1, 0);
+			send(watcher->fd, buffer_send, amount_read, 0);
 			printf("\n>>> ");
 		}
 	}
@@ -151,9 +181,6 @@ void client_callback(EV_P_ ev_io* watcher, int events){
 
 
 void accept_conn(EV_P_ ev_io* watcher, int events){
-
-	printf("Callback\n");
-	fflush(stdout);
 
 	int server_fd = watcher->fd;
 
@@ -171,7 +198,6 @@ void accept_conn(EV_P_ ev_io* watcher, int events){
 	ev_io_start(loop, client_watcher);
 
 	ev_io_stop(EV_A_ watcher);
-	ev_timer_stop(EV_A_ (ev_timer*) watcher->data);
 
 	printf("Connected to %s:%d\n", inet_ntoa(client_addr.sin_addr),
 			ntohs(client_addr.sin_port));
@@ -182,79 +208,94 @@ void accept_conn(EV_P_ ev_io* watcher, int events){
 
 void connect_conn(EV_P_ ev_timer* watcher, int events){
 
-	printf("Trie connect client\n");
+	printf("\nTrying to connect to remote server\n");
 	fflush(stdout);
 
 	connection_data* connection_server = (connection_data*) watcher->data;
 
-	int client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(connection_server->client_fd == -1){
 
-	make_non_blocking(client_fd);
+		connection_server->client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+		make_non_blocking(connection_server->client_fd);
+	}
 
 	struct sockaddr_in server_addr;
 	int result_connect;
 
 	client_set_addr(&server_addr, connection_server->server_ip, connection_server->server_port);
-	result_connect = connect(client_fd, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
+	result_connect = connect(connection_server->client_fd, (struct sockaddr*) &server_addr, sizeof(struct sockaddr_in));
+
 
 	if(result_connect < 0){
+
 		printf("Unable to connect to %s:%d\n", connection_server->server_ip, connection_server->server_port);
+
 	}
 	else{
+
 		client_watcher = (ev_io*) malloc(sizeof(ev_io));
 
-		printf("Connected as client!\n");
+		printf("Connection stablished!\n");
 
-		ev_io_stop(EV_A_ connection_server->this_server);
+		/* Stop timed connection */
 		ev_timer_stop(EV_A_ watcher);
 
-		ev_io_init(client_watcher, client_callback, client_fd, EV_WRITE | EV_READ);
+		/* Create and start event watcher for connected socket */
+		ev_io_init(client_watcher, client_callback, connection_server->client_fd, EV_WRITE | EV_READ);
 		ev_io_start(loop, client_watcher);
 
-		close(connection_server->this_server->fd);
 	}
 }
 
 
 int main(int argc, char** argv){
 
-	check_input(argc, argv);
+	bool server_mode = check_input(argc, argv);
 
 	make_non_blocking(0);
 
-	int server_fd;
-
-	char* other_server_ip = argv[1];
-	int other_server_port = atoi(argv[2]);
-	key = argv[3];
-
-	server_fd = open_server_socket(SERVER_PORT);
-
 	struct ev_loop* event_loop = EV_DEFAULT;
 
-	ev_io server_watcher;
-	ev_timer conector_watcher;
+	if(server_mode){
 
-	server_watcher.data = (void*) &conector_watcher;
+		key = argv[1];
 
-	ev_io_init(&server_watcher, accept_conn, server_fd, EV_READ);
-	ev_io_start(event_loop, &server_watcher);
+		int server_fd = open_server_socket(SERVER_PORT);
 
-	connection_data connection;
-	connection.server_ip = other_server_ip;
-	connection.server_port = other_server_port;
-	connection.this_server = &server_watcher;
+		ev_io server_watcher;
+		ev_io_init(&server_watcher, accept_conn, server_fd, EV_READ);
+		ev_io_start(event_loop, &server_watcher);
 
-	conector_watcher.data = (void*) &connection;
+	}
 
-	ev_timer_init(&conector_watcher, connect_conn, 0.0, 1.0);
-	ev_timer_start(event_loop, &conector_watcher);
+	else{
 
-	buffer = (char*) malloc(sizeof(char) * 64 KB);
+		char* other_server_ip = argv[1];
+		int other_server_port = atoi(argv[2]);
+		key = argv[3];
+
+		ev_timer conector_watcher;
+
+		connection_data connection;
+		connection.server_ip = other_server_ip;
+		connection.server_port = other_server_port;
+		connection.client_fd = -1;
+
+		conector_watcher.data = (void*) &connection;
+
+		ev_timer_init(&conector_watcher, connect_conn, 0.0, 1.0);
+		ev_timer_start(event_loop, &conector_watcher);
+
+	}
+
+	buffer_send = (char*) malloc(sizeof(char) * 64 KB);
+	buffer_recv = (char*) malloc(sizeof(char) * 64 KB);
 
 	ev_run(event_loop, 0);
 
-	free(buffer);
+	free(buffer_send);
+	free(buffer_recv);
 
 	return 0;
 
