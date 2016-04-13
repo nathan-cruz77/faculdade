34dd3cb1bac958a87034f1492c86526ac547c059
Nathan Cruz <nathan.cruz@unifesp.br>
2016-04-12 22:04:03 -0300

    Finalizada busca binaria, que ja deveria estar completa a muito tempo.

--- aed2/vez_2/pesquisa/busca_binaria.c
+++ aed2/vez_2/pesquisa/busca_binaria.c
@@ -7,15 +7,39 @@ int busca(int* A, int esq, int dir, int x){
     if(dir < esq){
         return -1;
     }
-    else if(dir == esq && A[esq] != x){
+
+	if(dir == esq && A[esq] != x){
         return -1;
     }
 
     if(A[meio] > x){
-        return busca(A, esq, )
+        return busca(A, esq, meio, x);
     }
+
+	if(A[meio] < x){
+		return busca(A, meio+1, dir, x);
+	}
+
+	return meio;
 }
 
 int main(){
+
+	int* A;
+	int n;
+	int i;
+
+	scanf("%d", &n);
+
+	A = malloc(sizeof(int) * n);
+
+	for(i = 0; i < n; i++){
+		scanf("%d", &(A[i]));
+	}
+
+	scanf("%d", &i);
+
+	printf("Busca binaria: %d\n", busca(A, 0, n - 1, i));
+
     return 0;
 }
